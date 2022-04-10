e73a6ae97060910707655c6716d7acfeea07f673
Alexander <70067346+rosjy@users.noreply.github.com>
2022-04-10 19:57:07 +0200

    added footer + responsive

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const stats = [90, 95, 85, 90, 70]
 AOS.init()
 
 $(document).ready(() => {
+    //hellos animation
     let word_i = 0
     setInterval(function() {
         $('#header-title').text(hello[word_i])
@@ -13,6 +14,42 @@ $(document).ready(() => {
         }
     }, 300);
 
+    //fetching other projects
+    fetch('https://api.github.com/users/turytsia/repos')
+        .then((response) => {
+            return response.json();
+        })
+        .then((repos) => {
+            for (const repo of repos) {
+                $('#projects-slider').append(`<li class = "projects-slider-item">
+                    <a target="_blank" href = "${repo.html_url}">
+                        <h4 class = "projects-slider-name">${repo.name}<i class="fa-brands fa-github"></i></h4>
+                        <p class = "projects-slider-text">${repo.description}</p>
+                    </a>
+                </li>`)
+            }
+            $('#feedback-slider').slick({
+                infinite: true,
+                centerMode: true,
+                centerPadding: '60px',
+                slidesToShow: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            })
+
+            $('#projects-slider').slick({
+                infinite: true,
+                centerMode: true,
+                centerPadding: '60px',
+                slidesToShow: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            })
+        }).catch(error => console.log(error));
+
+    //skill slider animation
     let stats_sliders = $('.stats-slider')
     let stats_anim_fl = false
     $(document).scroll(() => {
@@ -26,15 +63,16 @@ $(document).ready(() => {
         let el_counter = 0
         let percentage_counter = 0
         let i = setInterval(function() {
-            stats_sliders[el_counter].style.width = percentage_counter + '%'
-            stats_sliders[el_counter].nextElementSibling.innerHTML = percentage_counter + '%'
-            percentage_counter++;
-            if (percentage_counter > stats[el_counter]) {
-                percentage_counter = 0
-                el_counter++
-                if (el_counter > stats_sliders.length) {
-                    clearInterval(i)
+            if (el_counter < stats_sliders.length) {
+                stats_sliders[el_counter].style.width = percentage_counter + '%'
+                stats_sliders[el_counter].nextElementSibling.innerHTML = percentage_counter + '%'
+                percentage_counter++;
+                if (percentage_counter > stats[el_counter]) {
+                    percentage_counter = 0
+                    el_counter++
                 }
+            } else {
+                clearInterval(i)
             }
         }, 10);
     }
--- scripts/inertia.js
+++ scripts/inertia.js
@@ -1,48 +1,91 @@
-const body = document.body;
-const main = document.getElementById('inertia');
+$(document).ready(() => {
+    const levels_header = [document.getElementById('bg-level-4-header'),
+        document.getElementById('bg-level-3-header'),
+        document.getElementById('bg-level-2-header'),
+        document.getElementById('bg-level-1-header'),
+        document.getElementById('bg-level-0-header')
+    ]
+    const levels_footer = [document.getElementById('bg-level-4-footer'),
+        document.getElementById('bg-level-3-footer'),
+        document.getElementById('bg-level-2-footer'),
+        document.getElementById('bg-level-1-footer')
+    ]
 
-let sx = 0, // For scroll positions
-    sy = 0;
-let dx = sx, // For container positions
-    dy = sy;
+    const body = document.body;
+    const main = document.getElementById('inertia');
 
-
-body.style.height = main.clientHeight + 'px';
-
-
-main.style.position = 'fixed';
-main.style.top = 0;
-main.style.left = 0;
-
-// Bind a scroll function
-window.addEventListener('scroll', easeScroll);
-
-
-function easeScroll() {
-
-    sx = window.pageXOffset;
-    sy = window.pageYOffset;
-}
+    let sx = 0, // For scroll positions
+        sy = 0;
+    let dx = sx, // For container positions
+        dy = sy;
 
 
-window.requestAnimationFrame(render);
-
-function render() {
-    //We calculate our container position by linear interpolation method
-    dx = li(dx, sx, 0.07);
-    dy = li(dy, sy, 0.07);
-
-    dx = Math.floor(dx * 100) / 100;
-    dy = Math.floor(dy * 100) / 100;
+    body.style.height = main.clientHeight + 'px';
 
 
-    main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+    main.style.position = 'fixed';
+    main.style.top = 0;
+    main.style.left = 0;
 
+    // Bind a scroll function
+    window.addEventListener('scroll', easeScroll);
+
+
+    function easeScroll() {
+
+        sx = window.pageXOffset;
+        sy = window.pageYOffset;
+    }
 
 
     window.requestAnimationFrame(render);
-}
 
-function li(a, b, n) {
-    return (1 - n) * a + n * b;
-}
+    function render() {
+        const last_dy = dy
+            //We calculate our container position by linear interpolation method
+        dx = li(dx, sx, 0.07);
+        dy = li(dy, sy, 0.07);
+        dx = Math.floor(dx * 100) / 100;
+        dy = Math.floor(dy * 100) / 100;
+        if (last_dy !== dy) {
+            const footer_offset = footer.getBoundingClientRect().top
+                //header parallax
+            const speed = -dy
+
+            levels_header[0].style.transform = `translateY(${speed/3}px)`
+            levels_header[1].style.transform = `translateY(${speed/3.5}px)`
+            levels_header[2].style.transform = `translateY(${speed/4}px)`
+            levels_header[3].style.transform = `translateY(${speed/5.5}px)`
+            levels_header[4].style.transform = `translateY(${speed/8}px)`
+                //body
+            $('#about').css('transform', `translateY(${speed/5}px)`)
+            $('#skill').css('transform', `translateY(${speed/5}px)`)
+            $('#projects').css('transform', `translateY(${speed/5}px)`)
+            $('#header-transition').css('transform', `translateY(${speed/5}px)`)
+            $('#footer').css('transform', `translateY(${speed/4.9}px)`)
+            $('#skill-balloon').css('transform', `translateY(${speed/5}px)`)
+            $('#projects-balloon').css('transform', `translateY(${speed/5}px)`)
+            $('#footer-section').css('transform', `translateY(${speed/5}px)`)
+            $('#feedback').css('transform', `translateY(${speed/5}px)`)
+
+            const footer_speed = -(footer_offset - window.innerHeight / 2)
+
+            if (footer_offset - window.innerHeight <= 0) {
+                levels_footer[0].style.transform = `translateY(${footer_speed/25}px)`
+                levels_footer[1].style.transform = `translateY(${footer_speed/20}px)`
+                levels_footer[2].style.transform = `translateY(${footer_speed/10}px)`
+                levels_footer[3].style.transform = `translateY(${footer_speed/7}px)`
+            }
+        }
+
+        main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
+
+
+
+        window.requestAnimationFrame(render);
+    }
+
+    function li(a, b, n) {
+        return (1 - n) * a + n * b;
+    }
+})
